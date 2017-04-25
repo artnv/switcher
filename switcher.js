ec929464fc92c218e5c76bf7ba7ee34af5b72f14
Artyom Novikov <artnv@users.noreply.github.com>
2017-04-25 23:44:15 +0300

    add enemies method

--- switcher.js
+++ switcher.js
@@ -1,11 +1,97 @@
 var switcher = {};
+
+switcher.vars = {
+	_enemyArr: [],
+	_enemyElemCache: null,
+	_enemiesEnabled: false
+};
+
+// Вызывается во всех методах
+switcher.vars._enemyObserver = function(thatObj) {
+
+	var
+		vars 	= switcher.vars;
+		obj 	= vars._enemyArr,
+		i 		= vars._enemyArr.length,
+		cache	= vars._enemyElemCache;
+	
+	// Если есть кеш преыдущего объекта
+	if(vars._enemyElemCache) {
+		
+		vars._enemyElemCache.clearCache(); // очищаем кеш повторного нажатия, в предыдущем объекте из массива
+		
+		// Если это повторное нажатие, выходим
+		if(Object.getPrototypeOf(vars._enemyElemCache) === thatObj) {
+			return true;
+		}
+		
+	}
+	
+	// Обходим все объекты в массиве enemies
+	while(i--) {
+
+		// Если есть кеш
+		if(vars._enemyElemCache) {
+
+			// Если это тот элемент на котором кликнули
+			if(Object.getPrototypeOf(obj[i]) === thatObj) {
+
+				// Если не первый раз запускается, то выключаем элемент который ранее был включен
+				vars._enemyElemCache.turnOff();
+				vars._enemyElemCache = obj[i]; // кешируем текущий
+				
+				//break;
+			}
+
+		} else {
+			
+			vars._enemyElemCache = obj[i]; // кешируем текущий
+			
+		}
+
+	}
+
+};
+
+
+// Добавляет объекты в массив
+switcher.enemies = function() {
+	var
+		args 	= arguments,
+		i 		= args.length,
+		vars 	= switcher.vars,
+		arr		= vars._enemyArr;
+	
+	// Активизируем метод _enemyObserver
+	if(i > 0) {
+		vars._enemiesEnabled = true;
+	}
+	
+	// Добавляем элементы
+	while(i--) {
+		if(typeof args[i] === "object") {
+			arr.push(args[i]);
+		}
+	}
+
+};
+
+friends = function() {
+	
+};
+
 switcher.items = function (userConfig) {
 
 	// =============================== Vars
 	var 
 		R = {},
 		_cacheElement, // Для выключения элемента, который ранее был включен
-		_lock = false; // Включается методом .stop(). Если true, перестает реагировать на события, пока не будет вызван метод .next()
+		_lock = false, // Включается методом .stop(). Если true, перестает реагировать на события, пока не будет вызван метод .next()
+		
+		// enemies
+		_SwVars = switcher.vars,
+		_enemyObserver = _SwVars._enemyObserver;	// Наблюдатель
+
 		
 		// Конфиг
 		R._cfg = {
@@ -24,24 +110,15 @@ switcher.items = function (userConfig) {
 		R._tabs = {
 			parentElem: null,
 			arr: null,
-			turnOn: function() { this.style.fontWeight = "bold"; },
-			turnOff: function() { this.style.fontWeight = "normal"; }
+			turnOn: function() {},
+			turnOff: function() {}
 		};
 		
 	// end vars
 	
 	// =============================== Methods
 	
-	// Перестает реагировать на события, например пока анимация не завершится
-	R.stop = function () {
-		_lock = true;
-	};
-	
-	// Возобновляет реагирование на события
-	R.next = function () {
-		_lock = false;
-	};
-	
+
 	// Обработчик событий, который вызывает методы turnOn
 	R._listener = function () {
 		
@@ -67,7 +144,23 @@ switcher.items = function (userConfig) {
 		return document.getElementById(str.match(/#([\w\d_-]+)/i)[1]);
 	};
 	
+	
 	// =============================== Public Methods
+	
+	// Очищает кеш повторного нажатия, нужен для enemies
+	R.clearCache = function() {
+		_cacheElement = null;
+	};
+	
+	// Перестает реагировать на события, например пока анимация не завершится
+	R.stop = function () {
+		_lock = true;
+	};
+	
+	// Возобновляет реагирование на события
+	R.next = function () {
+		_lock = false;
+	};
 	
 	// Добавляет блоки
 	R.addBlocks = function (QSA, turnOn, turnOff) {
@@ -180,7 +273,7 @@ switcher.items = function (userConfig) {
 				
 				// Если повторный клик по тому же элементу
 				if(_cacheElement === i) {
-					
+				
 					// Если true, тогда при повторном нажатии на таб, применяется метод turnOff
 					if(cfg.backClickTurnOff) {
 						
@@ -191,9 +284,17 @@ switcher.items = function (userConfig) {
 					
 				} else {
 					
+					
 					// Выключает ранее включенный элемент
 					R.turnOff();
 
+					
+					// Если наблюдатель активирован (вызван метод enemies с аргументами - объектами), то запускаем
+					if(_SwVars._enemiesEnabled) {
+						_enemyObserver(this);
+					}
+					
+					
 					// Включение блоков
 					blocks.turnOn.call(blocks.arr[i]);
 					
@@ -201,10 +302,11 @@ switcher.items = function (userConfig) {
 					if(tabs.turnOn) {
 						tabs.turnOn.call(tabs.arr[i]);
 					}
-					
+
 					// Кешируем элемент который собираемся включить, чтобы потом его же и отключить
 					_cacheElement = i;
 					
+
 					// Выходим, т.к. нашли то что искали
 					break;
 				}
@@ -234,5 +336,5 @@ switcher.items = function (userConfig) {
 	
 	
 	// Новый экземпляр
-	return Object.create(R);	
+	return Object.create(R);
 };
